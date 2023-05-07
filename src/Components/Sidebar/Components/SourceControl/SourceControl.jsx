0e4f45c42c2818f5fb3359ae52daf3fe475d0e71
nekoify <xl83dev@gmail.com>
2023-05-08 01:21:37 +1000

    jjjjjjjjjj

--- src/Components/Sidebar/Components/SourceControl/SourceControl.jsx
+++ src/Components/Sidebar/Components/SourceControl/SourceControl.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-
+import React, {useRef} from 'react'
+import {pull, push, selectRepo} from "../../../../sourceControl"
 export default function SourceControl() {
   return (
     <div>
       <b>Source Control</b>
-      <div id="sc-main" class="nav-ele">
-      <a href="https://github.com/login/oauth/authorize?scope=repo&client_id=b6d2e4d50218cbda081b"><button id="sc-login">Log In</button></a>
-      <div id="sc-actions" hidden>
-      <button>Pull</button>
+      <div id="sc-main" class="nav-ele" >
+      <button onClick={pull}>Pull</button>
       <br/>
-      <button>Push</button>
-      </div>
+      <button onClick={push}>Push</button>
+      <br/>
+      <button onClick={selectRepo}>Select Repo</button>
       </div>
     </div>
   )
--- src/sourceControl.js
+++ src/sourceControl.js
@@ -2,152 +2,195 @@ import fetch from 'cross-fetch';
 var url = window.location.search
 const urlParams = new URLSearchParams(url);
 const code = urlParams.get('code')
-import Blockly from 'blockly/core';
+import Blockly from 'blockly';
 import base64 from 'base-64';
 import Swal from "sweetalert2";
 
 
 if (code) {
-    var changeUrl = new URL(document.location.href);
-    changeUrl.searchParams.delete('code');
-    window.history.replaceState({}, document.title, changeUrl);
-    if (!import.meta.env.VITE_SEC) {
-      alert("no .env sec :3")
-
-    }
-const body = {
-    "code": code,
-    "client_secret": import.meta.env.VITE_SEC,
-    "client_id":     "b6d2e4d50218cbda081b",
-    "redirect_uri":  "http://127.0.0.1:5173/",
-}
-
-fetch('https://s4d-api.xl83.dev/api/v1/user/githubsc/', {
+  var changeUrl = new URL(document.location.href);
+  changeUrl.searchParams.delete('code');
+  window.history.replaceState({}, document.title, changeUrl);
+  if (!
+    import.meta.env.VITE_SEC) {
+    alert("no .env secret found :( ask neko for secret so you can add it locally :3")
+  }
+  const body = {
+    "code": code
+  }
+  console.log(body)
+  fetch('https://s4d-api.xl83.dev/api/v1/user/githubsc/', {
     method: 'POST',
     mode: "no-cors",
-	body: JSON.stringify(body),
-	headers: {'Content-Type': 'application/json', "Accept": 'application/json'}
-}).then(res => {
-    res.json().then( res => {
-        var code = res.code
-        if (code) {
-            localStorage.setItem("accessToken", res.code)
-            localStorage.setItem("isTokenValid", "true")
-            updateGitUi("show")
-            fetch('https://api.github.com/user', {
-                headers: {
-                  Authorization: `token ${localStorage.getItem("accessToken")}`
-                }
-              })
-                .then(res => res.json())
-                .then(json => {
-                    console.log(json)
-                    localStorage.setItem("userData", JSON.stringify(json))
-                })
-        } else {
-            alert("There was a error, please refresh and try login again")
-          console.log(res)
-        }
-    })
-})
-
-} else {
-console.log("no code")
-    if (localStorage.getItem("accessToken")) {
-        fetch('https://api.github.com/user/repos', {
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      "Accept": 'application/json'
+    }
+  }).then(res => {
+    res.json().then(res => {
+      var code = res.code
+      if (code) {
+        localStorage.setItem("accessToken", res.code)
+        localStorage.setItem("isTokenValid", "true")
+        // updateGitUi("show")
+        console.log("plssss")
+        fetch('https://api.github.com/user', {
             headers: {
               Authorization: `token ${localStorage.getItem("accessToken")}`
             }
           })
-            .then(res => res.json())
-            .then(json => {
-                if (json.message == "Bad credentials") {
-                    localStorage.setItem("isTokenValid", "false")
-                    updateGitUi("hide")
-                } else {
-                    updateGitUi("show")
-                }
-            });
-    }
+          .then(res => res.json())
+          .then(json => {
+            console.log(json)
+            localStorage.setItem("userData", JSON.stringify(json))
+          })
+      } else {
+        alert("There was a error, please refresh and try login again")
+        console.log(res)
+      }
+    })
+  })
+
+} else {
+  console.log("no code")
+  if (localStorage.getItem("accessToken")) {
+    fetch('https://api.github.com/user/repos', {
+        headers: {
+          Authorization: `token ${localStorage.getItem("accessToken")}`
+        }
+      })
+      .then(res => res.json())
+      .then(json => {
+        if (json.message == "Bad credentials") {
+          localStorage.setItem("isTokenValid", "false")
+          //updateGitUi("hide")
+        } else {
+          //updateGitUi("show")
+        }
+      });
+  }
 }
 
-function updateGitUi(type) {
-    var ui = document.getElementById("sc-main")
-    if (type == "show") {
-       ui.style.visibility = "visible"
-    } else {
-       ui.style.visibility = "hidden"
-    }
-}
+//function updateGitUi(type) {
+//  if (type == "show") {
+//  thing
+//} else {
+// thing
+//  }
+//}
 
 function push() {
-var userDataString = localStorage.getItem("userData")
-var userData = JSON.parse(userDataString)
-var xmlDom = Blockly.Xml.workspaceToDom(Blockly.getMainWorkspace())
-var xml = Blockly.Xml.domToPrettyText(xmlDom)
+  var userDataString = localStorage.getItem("userData")
+  var userData = JSON.parse(userDataString)
+  var xmlDom = Blockly.Xml.workspaceToDom(Blockly.getMainWorkspace())
+  var xml = Blockly.Xml.domToPrettyText(xmlDom)
 
-var encodedContent = base64.encode(xml)
+  var encodedContent = base64.encode(xml)
 
-// Encode the file content as a base64 string
+  // Encode the file content as a base64 string
 
 
-// Get the SHA of the file
-fetch(`https://api.github.com/repos/${userData.login}/${localStorage.getItem("repo")}/contents/blocks.xml`, {
-    headers: {
-        Authorization: `token ${localStorage.getItem("accessToken")}`
-    }
-})
-  .then(response => response.json())
-  .then(fileData => {
-    const sha = fileData.sha;
-
-    // Send a PUT request to update the file
-    fetch(fileData.url, {
-      method: 'PUT',
+  // Get the SHA of the file
+  fetch(`https://api.github.com/repos/${userData.login}/${localStorage.getItem("repo")}/contents/blocks.xml?timestamp=${Date.now()}`, {
       headers: {
-        'Content-Type': 'application/json',
         Authorization: `token ${localStorage.getItem("accessToken")}`
-      },
-      body: JSON.stringify({
-        message: 'Updated file via S4D',
-        content: encodedContent,
-    sha: sha
-
-      })
+      }
     })
-    .then(response => {
-      if (response.ok) {
-        console.log('File updated successfully.');
-      } else {
-        response.json().then(resJson => {
-            console.log(resJson)
-            console.log(resJson.message)
+    .then(response => response.json())
+    .then(fileData => {
+      const sha = fileData.sha;
+      console.log(sha)
+      // Send a PUT request to update the file
+      fetch(fileData.url, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `token ${localStorage.getItem("accessToken")}`
+          },
+          body: JSON.stringify({
+            message: 'Updated file via S4D',
+            content: encodedContent,
+            sha: sha
+
+          })
+        })
+        .then(response => {
+          if (response.ok) {
+            console.log('File updated successfully.');
             Swal.fire({
+              icon: 'success',
+              title: 'Success',
+              text: "File updated successfully."
+            })
+          } else {
+            response.json().then(resJson => {
+              console.log(resJson)
+              console.log(resJson.message)
+              Swal.fire({
                 icon: 'error',
                 title: 'Error...',
                 text: resJson.message
               })
+            })
+          }
         })
-      }
-    })
-    .catch(error => console.log(error));
-  });
+        .catch(error => console.log(error));
+    });
 }
 
 function pull() {
-    var userDataString = localStorage.getItem("userData")
-    console.log(userDataString)
-    var userData = JSON.parse(userDataString)
-    fetch(`https://api.github.com/repos/${userData.login}/${localStorage.getItem("repo")}/contents/blocks.xml`, {
-                headers: {
-                  Authorization: `token ${localStorage.getItem("accessToken")}`
-                }
-            }).then(res => res.json())
-                .then(json => {
-               var xml = base64.decode(json.content)
-               let xmlDom = Blockly.Xml.textToDom(xml);
-               Blockly.Xml.domToWorkspace(xmlDom, Blockly.getMainWorkspace());
-            })
+  var userDataString = localStorage.getItem("userData")
+  console.log(userDataString)
+  var userData = JSON.parse(userDataString)
+  fetch(`https://api.github.com/repos/${userData.login}/${localStorage.getItem("repo")}/contents/blocks.xml?timestamp=${Date.now()}`, {
+      headers: {
+        Authorization: `token ${localStorage.getItem("accessToken")}`
+      }
+    }).then(res => res.json())
+    .then(json => {
+      var xml = base64.decode(json.content)
+      Blockly.getMainWorkspace().clear()
+      let xmlDom = Blockly.utils.xml.textToDom(xml);
+      Blockly.Xml.domToWorkspace(xmlDom, Blockly.getMainWorkspace());
+    })
 }
 
-export { push, pull }
+function selectRepo() {
+  var selectRepo = {}
+  fetch('https://api.github.com/user/repos', {
+      headers: {
+        Authorization: `token ${localStorage.getItem("accessToken")}`
+      }
+    })
+    .then(res => res.json())
+    .then(json => {
+      console.log(json)
+      json.forEach((ele) => {
+        var name = ele.name
+        console.log(name)
+        var newObj = {
+          [name]: ele.name
+        }
+        Object.assign(selectRepo, newObj)
+      });
+      Swal.fire({
+        title: "Repo Selection",
+        input: 'select',
+        inputOptions: selectRepo,
+        inputPlaceholder: 'Select a Option',
+        showCancelButton: true,
+      }).then(async (result) => {
+        localStorage.setItem("repo", result.value)
+        Swal.fire({
+          title: `Succesfully Selected ${result.value}!`,
+        })
+      })
+    })
+}
+
+export {
+  push,
+  pull,
+  selectRepo
+}
