cab6835828679ae981d117ddc52e811583cec07b
Retro <htoomyataung382@gmail.com>
2023-05-08 10:46:18 +0630

    Finally it looks good

--- src/Components/Sidebar/Components/Account/Account.jsx
+++ src/Components/Sidebar/Components/Account/Account.jsx
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from 'react'
-import {oauth} from "../../../../sourceControl"
+import { oauth } from "../../../../sourceControl"
 
 function UserCard({ userData }) {
   return (
-    <div className="w-[80%] h-72 border-2 pt-5 border-white rounded-lg mx-auto">
+    <div className="w-[80%] h-64 border-2 pt-5 border-white rounded-lg mx-auto">
       <img src={userData.avatar_url} alt={`${userData.login} avatar`} className="mx-auto rounded-full w-[70%] mb-2" />
       <h1 className="text-center text-2xl truncate text-white font-semibold">{userData.name}</h1>
       <p className="text-center text-zinc-400 text-lg truncate">{userData.login}</p>
 
-      <button
-        className="mt-3 mx-auto"
-        onClick={() => {
-          localStorage.removeItem("userData")
+      <div className="w-full flex h-auto justify-center">
+        <button
+          className="mt-3 mx-auto"
+          onClick={() => {
+            localStorage.removeItem("userData")
 
-          window.location.reload()
-        }}
-      >
-        Logout
-      </button>
+            window.location.reload()
+          }}
+        >
+          Logout
+        </button>
+      </div>
     </div>
   )
 }
@@ -28,7 +30,7 @@ export default function Account() {
   useEffect(() => {
     const userData = localStorage.getItem("userData")
 
-    if(!userData) return setData(
+    if (!userData) return setData(
       <div className="nav-ele">
         <button id="sc-login" onClick={oauth}>Log In with GitHub</button>
       </div>
