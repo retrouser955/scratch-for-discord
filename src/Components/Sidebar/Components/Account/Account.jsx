0f73aff9a3a899dcfd69a1594c679fc61b37c840
Retro <htoomyataung382@gmail.com>
2023-05-08 10:36:30 +0630

    test v2

--- src/Components/Sidebar/Components/Account/Account.jsx
+++ src/Components/Sidebar/Components/Account/Account.jsx
@@ -5,8 +5,17 @@ function UserCard({ userData }) {
   return (
     <div className="w-[80%] h-60 border-2 pt-5 border-white rounded-lg mx-auto">
       <img src={userData.avatar_url} alt={`${userData.login} avatar`} className="mx-auto rounded-full w-[70%] mb-2" />
-      <h1 className="text-center text-2xl truncate text-white font-semibold">{userData.login}</h1>
-      <p className="text-center text-zinc-400 text-lg">{userData.name}</p>
+      <h1 className="text-center text-2xl truncate text-white font-semibold">{userData.name}</h1>
+      <p className="text-center text-zinc-400 text-lg">{userData.login}</p>
+
+      <button
+        id="sc-login"
+        onClick={() => {
+          localStorage.removeItem("userData")
+
+          window.location.reload()
+        }}
+      >Logout</button>
     </div>
   )
 }
